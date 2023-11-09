9b4812e3dc02986f268badad74f701b9abb28691
Breno Arreguy <brenoarreguy@gmail.com>
2023-11-09 16:43:47 -0300

    Add solution to 3/30 Hello world

---
+++ hello_world.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char** argv) 
+{
+  // Initialisation
+  MPI_Init(&argc, &argv);
+
+  // Reading size and rank
+  int size, rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  // Printing
+  printf("Hello world, from process #%d\n", rank);
+
+  // Finalisation
+  MPI_Finalize();
+
+  return 0;
+}
